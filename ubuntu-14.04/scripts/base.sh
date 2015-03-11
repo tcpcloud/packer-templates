7a67b7b3a7919817bd7c467a6f1d56db6d3413b3
jan kaufman <jan.kaufman@tcpcloud.eu>
2015-03-11 14:16:32 +0100

    add curl and cloud-init packages

--- ubuntu-14.04/scripts/base.sh
+++ ubuntu-14.04/scripts/base.sh
@@ -1,7 +1,10 @@
+# TODO: move this to packages.sh
 apt-get update
 apt-get -y upgrade
 apt-get -y dist-upgrade
 apt-get -y install linux-headers-$(uname -r)
+apt-get -y install curl
+apt-get -y install cloud-init
 
 # to be removed
 #sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
