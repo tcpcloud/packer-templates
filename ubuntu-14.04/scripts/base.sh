750c4b395f53b4e480cedb525292c10dad8a29e4
jan kaufman <jan.kaufman@tcpcloud.eu>
2015-03-11 14:58:11 +0100

    removed package cloud-init till it's configured

--- ubuntu-14.04/scripts/base.sh
+++ ubuntu-14.04/scripts/base.sh
@@ -4,7 +4,7 @@ apt-get -y upgrade
 apt-get -y dist-upgrade
 apt-get -y install linux-headers-$(uname -r)
 apt-get -y install curl
-apt-get -y install cloud-init
+# apt-get -y install cloud-init
 
 # to be removed
 #sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
