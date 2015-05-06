0fcf1a705b5ef0845dc9c626190ae9d6e664b601
jan kaufman <jan.kaufman@tcpcloud.eu>
2015-05-06 10:16:12 +0200

    cloud-init support

--- ubuntu-14.04/scripts/base.sh
+++ ubuntu-14.04/scripts/base.sh
@@ -5,7 +5,9 @@ apt-get -y dist-upgrade
 apt-get -y install linux-headers-$(uname -r)
 apt-get -y install curl
 
-# apt-get -y install cloud-init
+apt-get -y install cloud-init
+chown root:ubuntu /etc/cloud/cloud.cfg.d
+chmod g+w /etc/cloud/cloud.cfg.d
 
 # to be removed
 #sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=sudo' /etc/sudoers
